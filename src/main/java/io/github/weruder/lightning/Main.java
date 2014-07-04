7198b629171bc5a4f40ca11a4962105a8b3b9476
weruder <myncijub@gmail.com>
2014-07-04 04:37:35 -0500

    Added code for fire on right click with stick

--- src/main/java/io/github/weruder/lightning/Main.java
+++ src/main/java/io/github/weruder/lightning/Main.java
@@ -1,20 +1,26 @@
 package io.github.weruder.lightning;
 
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public final class Main extends JavaPlugin {
+
 	@EventHandler
-	public void onPlayerUse(PlayerInteractEvent event) {
-	    Player player = event.getPlayer();
-	    if (player.getItemInHand().getType() == Material.STICK) {
-	        // Creates a bolt of lightning at a given location. In this case, that location is where the player is looking.
-	        // Can only create lightning up to 200 blocks away.
-	        player.getWorld().strikeLightning(BlockIterator(player, 200).getLocation());
-	    }
+	(priority=EventPriority.HIGH) 
+	public void onPlayerUse(PlayerInteractEvent event){
+		Player p = event.getPlayer();
+		if(event.getAction().equals(Action.RIGHT_CLICK_AIR)){
+			if(p.getItemInHand().getType() == Material.STICK){
+				Fireball fire = p.getWorld().spawn(event.getPlayer().getLocation().add(new Vector(0.0D, 1.0D, 0.0D)), Fireball.class); fire.setFireTicks(0); fire.setShooter(p); } 
+			else if(p.getItemInHand().getType() == Material.STICK){ //Do whatever } } }
+			}
+		}
 	}
 }
