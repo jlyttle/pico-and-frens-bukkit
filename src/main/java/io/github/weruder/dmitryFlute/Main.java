2d897d502b922a0c523461882c4bd3d68d674e1b
weruder <myncijub@gmail.com>
2014-07-04 13:56:33 -0500

    Adding twitter mod

---
+++ src/main/java/io/github/weruder/dmitryFlute/Main.java
@@ -0,0 +1,9 @@
+package io.github.weruder.dmitryFlute;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener 
+{
+	
+}
