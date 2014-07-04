1547cfbaa2e17573674713fa4e8456a191733c39
picoriley <acloudy@mail.smu.edu>
2014-07-04 02:45:04 -0700

    Added a few debugging statements

--- src/main/java/io/github/weruder/lightning/Main.java
+++ src/main/java/io/github/weruder/lightning/Main.java
@@ -15,12 +15,26 @@ public final class Main extends JavaPlugin {
 	@EventHandler
 	(priority=EventPriority.HIGH) 
 	public void onPlayerUse(PlayerInteractEvent event){
+		getLogger().info("Player has used an item!");
 		Player p = event.getPlayer();
 		if(event.getAction().equals(Action.RIGHT_CLICK_AIR)){
+		getLogger().info("Air has been right-clicked!");
 			if(p.getItemInHand().getType() == Material.STICK){
-				Fireball fire = p.getWorld().spawn(event.getPlayer().getLocation().add(new Vector(0.0D, 1.0D, 0.0D)), Fireball.class); fire.setFireTicks(0); fire.setShooter(p); } 
-			else if(p.getItemInHand().getType() == Material.STICK){ //Do whatever } } }
+				getLogger().info("BLASTIN'!");
+				Fireball fire = p.getWorld().spawn(event.getPlayer().getLocation().add(new Vector(0.0D, 1.0D, 0.0D)), Fireball.class); 
+				fire.setFireTicks(0); 
+				fire.setShooter(p); 
 			}
 		}
+	}
+
+	@Override
+	public void onEnable() {
+		getLogger().info("onEnable has been invoked!");
+	}
+ 
+	@Override
+	public void onDisable() {
+		getLogger().info("onDisable has been invoked!");
 	}
 }
