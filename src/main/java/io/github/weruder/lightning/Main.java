1df7b93c197c27ddf254413b8337bb167279ad8a
weruder <myncijub@gmail.com>
2014-07-03 20:11:52 -0500

    BlockIterator

--- src/main/java/io/github/weruder/lightning/Main.java
+++ src/main/java/io/github/weruder/lightning/Main.java
@@ -1,20 +1,20 @@
 package io.github.weruder.lightning;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void onPlayerInteractBlock(PlayerInteractEvent event) {
+	public void onPlayerUse(PlayerInteractEvent event) {
 	    Player player = event.getPlayer();
 	    if (player.getItemInHand().getType() == Material.STICK) {
 	        // Creates a bolt of lightning at a given location. In this case, that location is where the player is looking.
 	        // Can only create lightning up to 200 blocks away.
-	        player.getWorld().strikeLightning(player.getTargetBlock(null, 200).getLocation());
+	        player.getWorld().strikeLightning(BlockIterator(player, 200).getLocation());
 	    }
 	}
 }
